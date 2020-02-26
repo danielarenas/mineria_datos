998f5a9fb682584513f8dca4ab1b8d0627473e99
Armando <jesus.aguirre@tectijuana.edu.mx>
2020-02-26 14:45:49 -0800

    se agregaron las 10 funciones mas de la practica 10

--- Practics/Practica2.R
+++ Practics/Practica2.R
@@ -1,4 +1,4 @@
-#1Rnorm ejecuta numero entre intervalos del -1 al 1
+# [ 1 ] Rnorm ejecuta numero entre intervalos del -1 al 1
 df <- tibble::tibble(
   a = rnorm(10),
   b = rnorm(10),
@@ -15,38 +15,101 @@ df$c <- (df$c - min(df$c, na.rm = TRUE)) /
 df$d <- (df$d - min(df$d, na.rm = TRUE)) /
   (max(df$d, na.rm = TRUE) - min(df$d, na.rm = TRUE))
 
-#2 Guarda todos los valores en x y le sacas el minimo maximo.
+# [ 2 ] Guarda todos los valores en x y le sacas el minimo maximo.
 x <- df$a
 (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
 
-#3Crear un histograma en grafico
+# [ 3 ] Crear un histograma en grafico
 hist(rnorm(200)) 
 
-#4Mostrar todos los valores desglozados de x para crear un diagrama de densidad
+# [ 4 ] Mostrar todos los valores desglozados de x para crear un diagrama de densidad
 density(x)
 
-#5Crear un diagrama de densidad
+# [ 5 ] Crear un diagrama de densidad
 plot(density(x))
 
-#6 Sobre poner un histograma en un diagrama de densidad
+# [ 6 ] Sobre poner un histograma en un diagrama de densidad
 hist(x, freq = F)
 lines(density(x))
 
-
-#7 funcion en inversa
+# [ 7 ] funcion en inversa
 inverso <- function(x) {
   x <- 1 / x
   return(x)
 }
 inverso(8)
-#8 inverso
+
+# [ 8 ] inverso
 z <- c(1,2,3)
 t <- c("uno","dos","tres")
 z; t
 z
-#9 Asignar valores de numeros y en numbres
+
+# [ 9 ] Asignar valores de numeros y en numbres
 y <- c(2,4,6)  # Asigna 3 valores a la variable x
 names(y) <- c("I","II","III")  # Asigna nombres a los 3 valores anteriores
 y
-#10 calcula la raiz
+
+# [ 10 ] calcula la raiz
 sqrt(9)
+
+# ---------------------------------------------
+data(InsectSprays)
+aov.spray <- aov(sqrt(count)~spray,data=InsectSprays)
+aov.spray <- aov(sqrt(InsectSprays[,1])~InsectSprays[,2]) 
+
+# [ 11 ] The aov or lm function returns a list where the different 
+# elements corresponding to the results of analasis.
+str(aov.spray, max.level = -1)
+
+# [ 12 ] Display the names of the object.
+names(aov.spray)
+
+# [ 13 ] Create a list in which, in the case of aov (), it is simply a test table.
+str(summary(aov.spray))
+
+# [ 14 ] Returns the estimated coefficients (sometimes with standard errors).
+str(coef(aov.spray))
+
+# [ 15 ] Returns the residuals.
+str(residuals(aov.spray))
+
+# [ 16 ] Returns the deviation.
+str(deviance(aov.spray))
+
+# [ 17 ] Returns the adjusted values.
+str(fitted(aov.spray))
+
+# [ 18 ] calculate the factorial of a number
+factorial <- function(x,y) {
+  prod(1:x)
+  }
+factorial(4)
+
+# [ 19 ] function that adds two numbers
+sumanumeros <- function(x,y) {
+  (x+y)
+}
+sumanumeros(4,2)
+
+# [ 20 ] Function that compares two vectors, returns vector with the largest values.
+grande <- function(x,y){
+  y.g<-y>x
+  x[y.g]<-y[y.g]
+  x
+}
+grande(c(9,8,7,9,9),c(1,6,2,7,3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
