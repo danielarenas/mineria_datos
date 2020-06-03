33e5c70471b0abb11dc21b0a75812b8e47e1be0d
Armando <jesus.aguirre@tectijuana.edu.mx>
2020-06-03 13:50:20 -0700

    update practic1

--- Practics/Practica_1.r
+++ Practics/Practica_1.r
@@ -1,20 +1,20 @@
 getwd()
-setwd("/home/danie/Documentos/mineria_datos/P2-Movie-Ratings.csv")
+setwd("/home/armando/github/Equipo/mineria_datos/Practics")
 getwd()
 
 # Importing the dataset
-dataset <- read.csv('P2-Movie-Ratings.cs')
+dataset <- read.csv('P2-Movie-Ratings.csv')
 
 # Splitting the dataset into the Training set and Test set
-# Install.packages('caTools)
+# install.packages('caTools')
 library(caTools)
 set.seed(123)
-split <- sample.split(dataset$Budget (million $), SplitRatio = 2/3)
+split <- sample.split(dataset$Budget..million..., SplitRatio = 2/3)
 training_set <- subset(dataset, split == TRUE)
 test_set <- subset(dataset, split == FALSE)
 
 # Fitting Simple Linear Regression to the Training set
-regressor = lm(formula = Budget (million $) ~ Year of release,
+regressor = lm(formula = Budget..million... ~ Year.of.release,
                data = dataset)
 summary(regressor)
 
@@ -24,20 +24,20 @@ y_pred = predict(regressor, newdata = test_set)
 # Visualising the Training set results
 library(ggplot2)
 ggplot() +
-  geom_point(aes(x=training_set$Year of release, y=training_set$Budget (million $)),
+  geom_point(aes(x=training_set$Year.of.release, y=training_set$Budget..million...,
              color = 'red') +
-  geom_line(aes(x = training_set$Year of release, y = predict(regressor, newdata = training_set)),
+  geom_line(aes(x = training_set$Year.of.release, y = predict(regressor, newdata = training_set)),
             color = 'blue') +
   ggtitle('Year of release vs Budget (Training Set)') +
-  xlab('Year of release') +
-  ylab('Budget (million $')
+  xlab('Year.of.release') +
+  ylab('Budget..million...'))
 
 # Visualising the Test set results
 ggplot() +
-  geom_point(aes(x=test_set$Year of release, y=test_set$Budget (million $),
+  geom_point(aes(x=test_set$Year.of.release, y=test_set$Budget..million...,
              color = 'red') +
-  geom_line(aes(x = training_set$Year of release, y = predict(regressor, newdata = training_set)),
+  geom_line(aes(x = training_set$Year.of.release, y = predict(regressor, newdata = training_set)),
             color = 'blue') +
   ggtitle('Year of release vs Experience (Test Set)') +
-  xlab('Years of experience') +
-  ylab('Budget (million $)')
+  xlab('Years.of.experience') +
+  ylab('Budget..million...'))
